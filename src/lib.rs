369e224b4a82fe9e308fa65449857c604f0bd748
Tomas Korcak <korczis@gmail.com>
2018-03-08 21:40:19 +0100

    Simple add function

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,17 @@
+use std::ops::Add;
+
+pub fn add<T>(a: T, b: T) -> T::Output
+    where T: Add
+{
+    a + b
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(add(2, 2), 4);
     }
 }
