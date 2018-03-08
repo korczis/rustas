5a48145d613de18f450fe9b8fe04e7920115f109
Tomas Korcak <korczis@gmail.com>
2018-03-08 21:48:35 +0100

    Rust assembly

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,23 @@
+#![feature(asm)]
+
 use std::ops::Add;
 
-pub fn add<T>(a: T, b: T) -> T::Output
-    where T: Add
-{
-    a + b
+pub fn add(a: i32, b: i32) -> i32 {
+    let c: i32;
+    unsafe {
+        asm!("add $2, $0"
+             : "=r"(c)
+             : "0"(a), "r"(b)
+             );
+    }
+    c
 }
+
+//pub fn add<T>(a: T, b: T) -> T::Output
+//    where T: Add
+//{
+//    a + b
+//}
 
 #[cfg(test)]
 mod tests {
